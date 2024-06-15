911154406082a5f46d6f6fa2e844faf6abd721e5
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2024-06-15 22:38:48 +0100

    product id

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Products from "./Products";
+import Product from "./Product";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <main className="">
       <Header />
-      <Products />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
     </main>
   );
 }
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,14 +1,34 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Products() {
   const api = "https://fakestoreapi.com/products";
 
   const [data, setData] = useState([]);
 
+  // useEffect(() => {
+  //   fetch(api)
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data));
+  // }, []);
+
+  // async/await
   useEffect(() => {
-    fetch(api)
-      .then((response) => response.json())
-      .then((data) => setData(data));
+    const fetchData = async () => {
+      const response = await fetch(api);
+
+      const data = await response.json();
+
+      setData(data);
+
+      if (response.ok) {
+        console.log("Data fetched successfully");
+      } else {
+        console.log("Cannot fetch data");
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -16,21 +36,23 @@ function Products() {
       <h1 className="text-center text-4xl">Products</h1>
       <section className="flex flex-wrap justify-around gap-10 mt-10">
         {data.map((product, index) => (
-          <figure
-            className="flex flex-col justify-center px-5 items-center min-h-60 w-60 shadow-lg"
-            key={index}
-          >
-            <img
-              src={product.image}
-              alt="product's image"
-              className="w-full h-1/2"
-            />
-            <figcaption>
-              <span>{product.title}</span>
-              <br />
-              <strong>Price: ${product.price}</strong>
-            </figcaption>
-          </figure>
+          <Link to={`/products/${product.id}`}>
+            <figure
+              className="flex flex-col justify-center px-5 items-center min-h-60 w-60 shadow-lg"
+              key={index}
+            >
+              <img
+                src={product.image}
+                alt="product's image"
+                className="w-full h-1/2"
+              />
+              <figcaption>
+                <span>{product.title}</span>
+                <br />
+                <strong>Price: ${product.price}</strong>
+              </figcaption>
+            </figure>
+          </Link>
         ))}
       </section>
     </section>
