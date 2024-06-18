cd9e5b266c6245030f82361da7c95da71ee71efa
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2024-06-18 22:40:24 +0100

    added updates

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,6 +1,10 @@
-function Options({ item }) {
+function Options({ item, key, onClick, answer }) {
   return (
-    <button className="text-sm p-2 border-black border-[1px] rounded-3xl w-[40%] h-20">
+    <button
+      className={`text-sm p-2 border-black border-[1px] rounded-3xl w-[40%] h-20 ${answer}`}
+      key={key}
+      onClick={onClick}
+    >
       {item}
     </button>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,9 +5,14 @@ function Quiz() {
     "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple";
 
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [answer, setAnswer] = useState("");
+  const [wrongAnswer, setWrongAnswer] = useState("");
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   useEffect(() => {
     const fetchQuiz = async () => {
+      // setLoading(true);
       const response = await fetch(api);
 
       const result = await response.json();
@@ -19,6 +24,8 @@ function Quiz() {
           answer: item.correct_answer,
         };
       });
+
+      setLoading(false);
       setData(quiz);
 
       console.log(data);
@@ -26,19 +33,47 @@ function Quiz() {
 
     fetchQuiz();
   }, []);
-  return (
-    <section className="border-black border-2 w-[50vw] h-[80vh] shadow-lg rounded-[30px]">
-      <section className="h-[40%] flex justify-center items-center">
-        <h1 className="text-3xl">{data[0].question}</h1>
-      </section>
 
-      {/* //options */}
-      <section className="border-t-2 flex-wrap h-[60%] border-black flex justify-around items-center">
-        {data[0].options.map((item) => (
-          <Options item={item} />
-        ))}
-      </section>
-    </section>
+  const handleClick = (item, index) => {
+    if (item === data[currentQuestion].answer) {
+      setAnswer("bg-green-600 text-white");
+      console.log(index);
+    } else {
+      setWrongAnswer("bg-red-600");
+    }
+    setTimeout(() => {
+      setAnswer("");
+      setCurrentQuestion((prev) => prev + 1);
+    }, 2000);
+  };
+
+  return (
+    <>
+      {loading && <h1>loading...</h1>}
+      {!loading && (
+        <section className="border-[black] border-2 w-[50vw] h-[80vh] shadow-lg rounded-[30px]">
+          <section className="h-[40%] flex justify-center items-center">
+            <h1 className="text-3xl text-center">
+              {data[currentQuestion].question}
+            </h1>
+          </section>
+
+          {/* //options */}
+          <section className="border-t-2 flex-wrap h-[60%] border-black flex justify-around items-center">
+            {data[currentQuestion].options.map((item, index) => (
+              <Options
+                item={item}
+                key={index}
+                onClick={() => handleClick(item, index)}
+                answer={
+                  item === data[currentQuestion].answer ? answer : wrongAnswer
+                }
+              />
+            ))}
+          </section>
+        </section>
+      )}
+    </>
   );
 }
 
